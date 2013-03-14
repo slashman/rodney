70a7c304c71ea8bd838b0096c8d74dc55f7349df
slash@slashware.net <slash@slashware.net@9724f4aa-2c73-cbe1-17b6-4544a9f8dc26>
2013-03-14 05:39:10 +0000

    Add some color Lowercase for small enemies

--- js/Tiles.js
+++ js/Tiles.js
@@ -5,31 +5,31 @@ function Tiles(){
 
 	//Monsters
 	this.addTile("GIANT_ANT", new ut.Tile("A", 255, 255, 255));
-	this.addTile("BAT", new ut.Tile("B", 255, 255, 255));
-	this.addTile("CENTAUR", new ut.Tile("C", 255, 255, 255));
-	this.addTile("DRAGON", new ut.Tile("D", 255, 255, 255));
-	this.addTile("FLOATING_EYE", new ut.Tile("E", 255, 255, 255));
-	this.addTile("VIOLET_FUNGI", new ut.Tile("F", 255, 255, 255));
-	this.addTile("GNOME", new ut.Tile("G", 255, 255, 255));
-	this.addTile("HOBGOBLIN", new ut.Tile("H", 255, 255, 255));
+	this.addTile("BAT", new ut.Tile("b", 170, 170, 170));
+	this.addTile("CENTAUR", new ut.Tile("C", 170, 55, 0));
+	this.addTile("DRAGON", new ut.Tile("D", 170, 0, 0));
+	this.addTile("FLOATING_EYE", new ut.Tile("e", 0, 170, 170));
+	this.addTile("VIOLET_FUNGI", new ut.Tile("F", 255, 55, 255));
+	this.addTile("GNOME", new ut.Tile("g", 0, 170, 0));
+	this.addTile("HOBGOBLIN", new ut.Tile("H", 170, 170, 170));
 	this.addTile("INVISIBLE_STALKER", new ut.Tile("I", 255, 255, 255));
-	this.addTile("JACKAL", new ut.Tile("J", 255, 255, 255));
-	this.addTile("KOBOLD", new ut.Tile("K", 255, 255, 255));
-	this.addTile("LEPRECHAUN", new ut.Tile("L", 255, 255, 255));
-	this.addTile("MIMIC", new ut.Tile("M", 255, 255, 255));
-	this.addTile("NYMPH", new ut.Tile("N", 255, 255, 255));
-	this.addTile("ORC", new ut.Tile("O", 255, 255, 255));
-	this.addTile("PURPLE_WORM", new ut.Tile("P", 255, 255, 255));
-	this.addTile("QUASIT", new ut.Tile("Q", 255, 255, 255));
-	this.addTile("RUST_MONSTER", new ut.Tile("R", 255, 255, 255));
-	this.addTile("SNAKE", new ut.Tile("S", 255, 255, 255));
-	this.addTile("TROLL", new ut.Tile("T", 255, 255, 255));
-	this.addTile("UMBER_HULK", new ut.Tile("U", 255, 255, 255));
-	this.addTile("VAMPIRE", new ut.Tile("V", 255, 255, 255));
-	this.addTile("WRAITH", new ut.Tile("W", 255, 255, 255));
-	this.addTile("XORN", new ut.Tile("X", 255, 255, 255));
+	this.addTile("JACKAL", new ut.Tile("j", 255, 255, 55));
+	this.addTile("KOBOLD", new ut.Tile("k", 170, 55, 0));
+	this.addTile("LEPRECHAUN", new ut.Tile("l", 0, 170, 0));
+	this.addTile("MIMIC", new ut.Tile("m", 170, 0, 0));
+	this.addTile("NYMPH", new ut.Tile("n", 55, 55, 255));
+	this.addTile("ORC", new ut.Tile("o", 0, 170, 0));
+	this.addTile("PURPLE_WORM", new ut.Tile("P", 255, 55, 255));
+	this.addTile("QUASIT", new ut.Tile("q", 170, 0, 0));
+	this.addTile("RUST_MONSTER", new ut.Tile("R", 55, 255, 55));
+	this.addTile("SNAKE", new ut.Tile("s", 0, 170, 0));
+	this.addTile("TROLL", new ut.Tile("T", 0, 170, 0));
+	this.addTile("UMBER_HULK", new ut.Tile("U", 55, 55, 55));
+	this.addTile("VAMPIRE", new ut.Tile("v", 55, 55, 55));
+	this.addTile("WRAITH", new ut.Tile("w", 55, 55, 55));
+	this.addTile("XORN", new ut.Tile("x", 170, 0, 0));
 	this.addTile("YETI", new ut.Tile("Y", 255, 255, 255));
-	this.addTile("ZOMBIE", new ut.Tile("Z", 255, 255, 255));
+	this.addTile("ZOMBIE", new ut.Tile("Z", 170, 55, 0));
 	
 	this.addTile("AT", new ut.Tile("@", 255, 255, 255));
 	
@@ -39,9 +39,9 @@ function Tiles(){
 	
 	
 	//Terrain
-	this.addTerrainTile("#", new ut.Tile('#', 100, 100, 100), true, true,false);
-	this.addTerrainTile(".",  new ut.Tile('.', 50, 50, 50), false, false,false);
-	this.addTerrainTile(">", new ut.Tile('>', 0, 255, 0), false, false, true);
+	this.addTerrainTile("#", new ut.Tile('#', 55, 55, 0), true, true,false);
+	this.addTerrainTile(".",  new ut.Tile('.', 0, 55, 0), false, false,false);
+	this.addTerrainTile(">", new ut.Tile('>', 170, 0, 0), false, false, true);
 }
 
 Tiles.prototype.addTile = function(tileId, tile){
@@ -58,7 +58,7 @@ Tiles.prototype.addTerrainTile = function(tileId, utTile, solid, opaque, downsta
 
 	// Add memory tile, shade of Red
 	var memoryTile = utTile.clone();
-	memoryTile.setColor(utTile.getColorJSON().r, 0, 0);
+	memoryTile.setColor(utTile.getColorJSON().g, 0, 0);
 	this.addTile("MEM_"+tileId, memoryTile);	
 };
 
